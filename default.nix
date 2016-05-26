5125c41203d9bd43505f6dd05b2c817732ac2b8c
Taylor "Nekroze" Lawson <nekroze.lawson@gmail.com>
2016-05-26 19:58:48 +1000

    Create default.nix

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+with nixpkgs;
+
+stdenv.mkDerivation rec {
+  name = "ldc-calypso-${version}";
+  version = "0.17.1";
+
+  src = fetchgit {
+    url = "https://github.com/Syniurge/Calypso.git";
+    rev = "648a62f15a2b9db98e077bc930ad34c2370f8fbe";
+    sha256 = "0c3zplwh7xh8fz3anfrvpaqd93x6ddvgn51y1q87sa06n0dw8x6v";
+    fetchSubmodules = true;
+  };
+
+  buildInputs = [cmake libconfig libxml2 llvm_36];
+
+  enableParallelBuilding = true;
+  cmakeFlags = [
+    "-DBUILD_SHARED_LIBS=ON"
+  ];
+
+  patches = [
+    ./math.patch
+  ];
+}
